1fadff9a09b732ab12124f84cbbf805f367b9d22
Amer Ibric <ameribric@gmail.com>
2022-05-23 15:06:16 -0400

    commit tree 9a9b45138790106605b6055b0462d26985bd7b16

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 4) do
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.integer "grade"
+    t.datetime "birthday"
+    t.string "hehe"
+    t.string "hello"
+  end
 
 end
